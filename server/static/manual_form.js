8096ba0a3294ca6552cf8ab8631d6ee5fcdcddf5
Shiry Ginosar <shiry@dhcp-44-72.eecs.berkeley.edu>
2012-11-15 15:42:21 -0800

    moving more

---
+++ server/static/manual_form.js
@@ -0,0 +1,3 @@
+
+/* usability enhancement ideas */
+document.getElementById('manual_percent_moisture').focus();
---
+++ server/static/tomato_chart.js
@@ -0,0 +1,57 @@
+var incoming_data;
+
+$(document).ready(function() {
+	// post the new instructions to the watering system
+	// TODO
+
+	// graph the new moisture readings
+	document.getElementById('tomato_chart_div').select();
+	updater.start();
+});
+
+var updater = {
+	socket: null,
+
+	start: function() {
+		var url = "ws://" + location.host + "/plant/plant_1";
+		if ("WebSocket" in window) {
+			updater.socket = new WebSocket(url);
+		} else {
+			updater.socket = new MozWebSocket(url);
+		}
+		updater.socket.onmessage = function(event) {
+			updater.updateIncoming(JASON.parse(event.data));
+		}
+	},
+
+	updateIncoming: function(incoming) {
+		// TODO what is the format of data coming in?
+		// TODO update the incoming data variable
+		incoming_data = incoming;
+	}
+};
+
+google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['time', 'ideal %moisture', 'recorded %moisture'],
+          ['Sun',  10,       10],
+          ['Mon',  100,      90],
+          ['Tue',  80,      90],
+          ['Wed',60,      70],
+          ['Thu',  20,     60],
+          ['Fri',  10,       50],
+          ['Sat', 10,      40]
+        ]);
+
+		// TODO go over the incoming data variable
+		 data.setCell(0,1,incoming_data);
+
+        var options = {
+          title: 'Tomatoes'
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('tomato_chart_div'));
+        chart.draw(data, options);
+      }
