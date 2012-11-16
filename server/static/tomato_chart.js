0b5fca6e4cc099b646e089c8ecab4fab81321ba2
Valkyrie Savage <valkyrie.savage@gmail.com>
2012-11-16 10:47:22 -0800

    chart can be drawn from server data

--- server/static/tomato_chart.js
+++ server/static/tomato_chart.js
@@ -19,6 +19,8 @@ var updater = {
 			updater.socket = new MozWebSocket(url);
 		}
 		updater.socket.onmessage = function(event) {
+                        // parse the incoming JSON into a JavaScript array
+                        // of Javascript Objects containig property,value pairs
 			updater.updateIncoming(JSON.parse(event.data));
 		}
 	},
@@ -34,6 +36,7 @@ var updater = {
 google.load("visualization", "1", {packages:["corechart"]});
       google.setOnLoadCallback(drawChart);
       function drawChart() {
+        /*
         var data = google.visualization.arrayToDataTable([
           ['time', 'ideal %moisture', 'recorded %moisture'],
           ['Sun',  10,       10],
@@ -44,9 +47,26 @@ google.load("visualization", "1", {packages:["corechart"]});
           ['Fri',  10,       50],
           ['Sat', 10,      40]
         ]);
+*/
+        if (typeof(incoming_data) == "undefined") return;
 
-	// TODO go over the incoming data variable
-	data.setCell(1,1,incoming_data);
+        var data = new google.visualization.DataTable();
+        // add columns
+        //data.addColumn('date', 'date'); TODO switch to date and format
+        data.addColumn('number', 'date');
+        data.addColumn('number', 'ideal %moisture');
+        data.addColumn('number', 'recorded %moisture');
+        // add empty rows
+        data.addRows(incoming_data.length)
+        // populate the rows
+	// go over the incoming data variable
+        for (var row = 0; row < incoming_data.length; ++row) {
+          for (var date in incoming_data[row]) {
+	    data.setCell(row, 0, parseFloat(date));
+            data.setCell(row, 1, 1);
+            data.setCell(row, 2, parseFloat(incoming_data[row][date]));
+          }
+        }
 
         var options = {
           title: 'Tomatoes'
